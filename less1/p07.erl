05ee54e7a8970b08300ce47c61412c8169600c20
Evgeny <x230@prog.local>
2019-06-26 10:04:34 +0300

    Modified module 07 for correct work with nested lists

--- less1/p07.erl
+++ less1/p07.erl
@@ -2,23 +2,21 @@
 -export([flatten/1]).
 -import(p05, [reverse/1]).
 
+
 flatten([]) ->
     [];
 
 flatten([X|Xs]) ->
-    flatten_start([X|Xs], []).
+    reverse(flatten_start([X|Xs], [])).
 
 
 flatten_start([], Acc) ->
-    reverse(Acc);
+    Acc;
 
+flatten_start([X=[_|_]|Xs], Acc) ->
+    flatten_start(Xs, flatten_start(X, Acc));
 
-%%need to change this function
-flatten_start([X=[Xh|Xt]|Xs], Acc) ->
-    flatten_start(Xt, [Xh|Acc]);
-
-
-flatten_start([X=[]|Xs], Acc) ->
+flatten_start([_=[]|Xs], Acc) ->
     flatten_start(Xs, Acc);
 
 flatten_start([X|Xs], Acc) ->
