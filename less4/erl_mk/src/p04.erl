d9c02e701ba001b79b2b07458ea001e794b975a2
djani2703 <djani2703@gmail.com>
2019-07-19 10:20:20 +0300

    Remove directory less4

--- less4/erl_mk/src/p04.erl
+++
@@ -1,9 +0,0 @@
--module(p04).
--export([len/1]).
-
-
-len([]) ->
-    0;
-
-len([_|Xs]) ->
-    1 + len(Xs).
--- less4/erl_mk/src/p14.erl
+++
@@ -1,9 +0,0 @@
--module(p14).
--export([duplicate/1]).
-
-
-duplicate([]) ->
-    [];
-
-duplicate([X|Xs]) ->
-    [X, X | duplicate(Xs)].
